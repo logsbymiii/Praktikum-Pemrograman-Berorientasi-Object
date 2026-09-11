53701715fb8688e14d1407b416efe020bb0c619b
helmi <137972846+teathricalhood122@users.noreply.github.com>
2026-09-11 14:22:02 +0700

    consturctor

--- Pertemuan3/Motor.java
+++ Pertemuan3/Motor.java
@@ -1,16 +1,15 @@
 
 public class Motor {
-    private String platNomor; //merubah modifier menjadi private
-    private boolean isMesinOn; //merubah modifier menjadi private
-    private int kecepatan; //merubah modifier menjadi private
+    private String platNomor; // merubah modifier menjadi private
+    private boolean isMesinOn; // merubah modifier menjadi private
+    private int kecepatan; // merubah modifier menjadi private
 
-    public void displayStatus(){
+    public void displayStatus() {
         System.out.println("Plat Nomor: " + this.platNomor);
 
         if (isMesinOn) {
             System.out.println("Mesin On");
-        }
-        else{
+        } else {
             System.out.println("Mesin Off");
         }
 
@@ -18,29 +17,35 @@ public class Motor {
         System.out.println("=========================");
     }
 
-    public String getPlatNomor() { //menambahkan method getter untuk platNomor
+    public String getPlatNomor() { // menambahkan method getter untuk platNomor
         return platNomor;
     }
 
-    public void setPlatNomor(String platNomor) { //menambahkan method setter untuk platNomor
+    public void setPlatNomor(String platNomor) { // menambahkan method setter untuk platNomor
         this.platNomor = platNomor;
     }
- 
-    public boolean isMesinOn() { //menambahkan method getter untuk isMesinOn
+
+    public boolean isMesinOn() { // menambahkan method getter untuk isMesinOn
         return isMesinOn;
     }
 
-    public void setMesinOn(boolean isMesinOn) { ///menambahkan method setter untuk isMesinOn
+    public void setMesinOn(boolean isMesinOn) { /// menambahkan method setter untuk isMesinOn
         this.isMesinOn = isMesinOn;
     }
 
-    public int getKecepatan() { //menambahkan method getter untuk kecepatan
+    public int getKecepatan() { // menambahkan method getter untuk kecepatan
         return kecepatan;
     }
 
-    public void setKecepatan(int kecepatan) { //menambahkan method setter untuk kecepatan
-        if (!this.isMesinOn && kecepatan > 0) { //menambahkan validasi untuk kecepatan jika mesin off
+    public void setKecepatan(int kecepatan) {
+        if (!this.isMesinOn && kecepatan > 0) {
             System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else if (kecepatan < 0) { // menambah validasi untuk kecepatan agar tidak kurang dari 0
+            System.out.print("Kecepatan tidak boleh kurang dari 0");
+            this.kecepatan = 0;
+        } else if (kecepatan > 100) {
+            System.out.print("Kecepatan tidak boleh lebih dari 100");
+            this.kecepatan = 100;
         } else {
             this.kecepatan = kecepatan;
         }
